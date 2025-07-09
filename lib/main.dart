fb85b528247372488acd990ed6bf5dea07fa151d
iPadKID27 <husvainglory@hotmail.com>
2025-07-09 17:54:01 +0700

    MaterialApp & Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,6 @@ void main() {
   runApp(const MyApp());
 }
 
-String name = "maksdjka";
-int number = 1;
-double ladjsj = 1.0;
-List mylist = ['saksdjka', 'ajshdgahsdb'];
-Map<String, dynamic> myMap = {"String": "String", "String2": 4444};
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -17,38 +11,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.deepPurple,
+          seedColor: Colors.teal,
           brightness: Brightness.dark,
         ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-        leading: Icon(Icons.account_circle, color: Colors.amber),
-      ),
-      body: Wrap(children: [
-        Text("askdjiasjd"),Text("askdjiasjd"),Text("askdjiasjd"),Text("askdjiasjd"),Text("askdjiasjd"),Text("askdjiasjd"),Text("askdjiasjd"),Text("askdjiasjd"),
-      ],),
+      home: Scaffold(),
     );
   }
 }
